1a24adc8712be1374cc5d34fc68ccd12916c7d94
Omar Marzook <omarmarzook92@gmail.com>
2018-08-14 21:24:20 +0200

    add the map & foursquare

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import MapApp from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<MapApp />, document.getElementById("root"));
