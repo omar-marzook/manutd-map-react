bc4a9bca7aac03eef7bc7a461b09381d369f75bc
Omar Marzook <omarmarzook92@gmail.com>
2018-08-15 14:41:35 +0200

    Prettier my code

--- src/App.js
+++ src/App.js
@@ -1,75 +1,77 @@
-import React, { Component } from 'react';
-import './index.css';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-import mapStyle from './map-style.json';
+import React, { Component } from "react";
+import "./index.css";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import mapStyle from "./map-style.json";
 
-const foursquare = require('react-foursquare')({
-  clientID: 'GOCZGJUBBXCSCUFF4QTUIH0FTXP35RXALJOFHL22DHVBGR5Q',
-  clientSecret: 'WKSOXP1FDMQI2V4P552VEVYO2XBPKQVKA114NUYFTBFWXH2P'
+const foursquare = require("react-foursquare")({
+  clientID: "GOCZGJUBBXCSCUFF4QTUIH0FTXP35RXALJOFHL22DHVBGR5Q",
+  clientSecret: "WKSOXP1FDMQI2V4P552VEVYO2XBPKQVKA114NUYFTBFWXH2P"
 });
 
 const params = {
-  'll': '53.4631,-2.29139',
-  'query': 'Manchester United',
-  'limit': '5'
+  ll: "53.4631,-2.29139",
+  query: "Manchester United",
+  limit: "5"
 };
 
 export class MapApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
-    };
+       items: [] 
+      };
   }
 
   componentDidMount() {
     foursquare.venues.getVenues(params)
-      .then(res => {
-        this.setState({ items: res.response.venues });
-      });
+    .then(res => {
+      this.setState({ items: res.response.venues });
+    });
   }
 
-    // componentDidMount() {
-    //   fetch('https://api.foursquare.com/v2/users/510483378/list/manutdmapreact')
-    //     .then(response => response.json())
-    //     .then((data) => {
-    //       this.setState({
-    //         venues: data.response.venues
-    //       })
-    //     })
-    //     .catch((error) => {
-    //       alert('Error getting data from the FourSquare API. Check the key.')
-    //       console.log('Error getting data from the FourSquare API. Check the key.')
-    //     })
-    // }
+  // componentDidMount() {
+  //   fetch('https://api.foursquare.com/v2/users/510483378/list/manutdmapreact')
+  //     .then(response => response.json())
+  //     .then((data) => {
+  //       this.setState({
+  //         venues: data.response.venues
+  //       })
+  //     })
+  //     .catch((error) => {
+  //       alert('Error getting data from the FourSquare API. Check the key.')
+  //       console.log('Error getting data from the FourSquare API. Check the key.')
+  //     })
+  // }
 
   render() {
-    const style = { width: '100%', height: '100%' };
+    const style = { width: "100%", height: "100%" };
     return (
       <div>
-        <header className='header-bar'>
+        <header className="header-bar">
           <h1>Manchester United Map</h1>
         </header>
-        <aside className='right-section'>
-            {this.state.items.map(item => { return <a key={item.id}>{item.name}</a> })}
+        <aside className="right-section">
+          {this.state.items.map(item => {
+            return (
+              <a key={item.id}>
+                {item.name} {item.location.lat} {item.location.lng}
+              </a>
+            );
+          })}
         </aside>
 
-        <div className='map-canvas'>
+        <div className="map-canvas">
           <Map
             google={this.props.google}
             style={style}
             styles={mapStyle}
-            initialCenter={{
-              lat: 53.4631,
-              lng: -2.29139
-            }}
+            initialCenter={{ lat: 53.4631, lng: -2.29139 }}
             zoom={15}
             onClick={this.onMapClicked}
           >
-            <Marker onClick={this.onMarkerClick} name={'Current location'} />
+            <Marker onClick={this.onMarkerClick} name={"Current location"} />
 
-            
-            <Marker onClick={this.onMarkerClick} name={'Current location'} />
+            <Marker onClick={this.onMarkerClick} name={"Current location"} />
 
             <InfoWindow onClose={this.onInfoWindowClose}>
               <div>
@@ -80,12 +82,13 @@ export class MapApp extends Component {
           </Map>
         </div>
       </div>
-
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDr2mpFQ0YiKrf8bW71BurYN_QIl6uylys',
-  v: '3.30'
+  apiKey: "AIzaSyDr2mpFQ0YiKrf8bW71BurYN_QIl6uylys",
+  v: "3.30"
 })(MapApp);
+
+//res.response.venues[1].location.lat
